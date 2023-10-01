a6aa355c8bfbbf33c9c96bf843018590c6b70251
karthik-mohan23 <karthikmohan2310@gmail.com>
2023-10-01 12:45:24 +0530

    set up routes to send single products data to client

--- backend/routes/productsRoute.js
+++ backend/routes/productsRoute.js
@@ -1,12 +1,23 @@
 const router = require("express").Router();
 const productsModel = require("../models/productModel");
 
+// to get all products
 router.get("/", async (req, res, next) => {
   try {
     const allProducts = await productsModel.find({});
     res.status(200).json(allProducts);
   } catch (error) {
-    res.json(error);
+    console.log(error);
+  }
+});
+
+// to get single product
+router.get("/:id", async (req, res, next) => {
+  try {
+    const singleProduct = await productsModel.findById(req.params.id);
+    res.status(200).json(singleProduct);
+  } catch (error) {
+    console.log(error);
   }
 });
 

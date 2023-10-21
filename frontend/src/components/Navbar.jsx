c32c0a65b251d5a8963954a2e2b3254b9253d3ad
karthik-mohan23 <karthikmohan2310@gmail.com>
2023-10-21 13:14:40 +0530

    added theme toggle functionality

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,9 +1,23 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
 import { Link } from "react-router-dom";
 import { numberOfItemsInCart, totalPrice } from "../features/cart/cartSlice";
 import { useAuthContext } from "../context/useAuthContext";
 import axios from "axios";
+
+// themes object
+const themes = {
+  dark: "forest",
+  light: "cupcake",
+};
+
+// get theme from local storage when component mounts
+const getThemeFromLocalStorage = () => {
+  // if theme is not present in local storage - use themes.dark
+  //   else - use get it from local storage
+  return localStorage.getItem("theme") || themes.dark;
+};
+
 const Navbar = () => {
   const cartLength = useSelector(numberOfItemsInCart);
   const amountToPay = useSelector(totalPrice);
@@ -19,29 +33,22 @@ const Navbar = () => {
     handleLogout;
   }, [handleLogout]);
 
+  const [theme, setTheme] = useState(getThemeFromLocalStorage());
+
+  const handleTheme = () => {
+    const { dark, light } = themes;
+    const newTheme = theme === dark ? light : dark;
+    setTheme(newTheme);
+  };
+
+  useEffect(() => {
+    // set data-theme attribute to root element
+    document.documentElement.setAttribute("data-theme", theme);
+    // save theme to local storage
+    localStorage.setItem("theme", theme);
+  }, [theme]);
+
   return (
-    // <header className="bg-base-100">
-    //   <nav className="flex justify-center  w-[90%] max-w-5xl mx-auto">
-    //     <div>
-    //       <Link
-    //         to="/admin/user-management"
-    //         className="btn btn-ghost normal-case text-xl">
-    //         User Management
-    //       </Link>
-    //       <Link
-    //         to="/admin/order-records"
-    //         className="btn btn-ghost normal-case text-xl">
-    //         Order Records
-    //       </Link>
-    //       <Link
-    //         to="/admin/product-management"
-    //         className="btn btn-ghost normal-case text-xl">
-    //         Product Management
-    //       </Link>
-    //       <a href="#">Logout</a>
-    //     </div>
-    //   </nav>
-    // </header>
     <header className=" bg-base-100 drop-shadow-md">
       <div className="navbar w-[90%] max-w-5xl mx-auto">
         <div className="navbar-start">
@@ -118,6 +125,27 @@ const Navbar = () => {
         </div>
         <div className="navbar-end">
           <div className="flex gap-4">
+            {/* Theme switch */}
+            <label className="swap swap-rotate">
+              {/* this hidden checkbox controls the state */}
+              <input type="checkbox" onChange={handleTheme} />
+
+              {/* sun icon */}
+              <svg
+                className="swap-on fill-current w-6 h-6"
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 24 24">
+                <path d="M5.64,17l-.71.71a1,1,0,0,0,0,1.41,1,1,0,0,0,1.41,0l.71-.71A1,1,0,0,0,5.64,17ZM5,12a1,1,0,0,0-1-1H3a1,1,0,0,0,0,2H4A1,1,0,0,0,5,12Zm7-7a1,1,0,0,0,1-1V3a1,1,0,0,0-2,0V4A1,1,0,0,0,12,5ZM5.64,7.05a1,1,0,0,0,.7.29,1,1,0,0,0,.71-.29,1,1,0,0,0,0-1.41l-.71-.71A1,1,0,0,0,4.93,6.34Zm12,.29a1,1,0,0,0,.7-.29l.71-.71a1,1,0,1,0-1.41-1.41L17,5.64a1,1,0,0,0,0,1.41A1,1,0,0,0,17.66,7.34ZM21,11H20a1,1,0,0,0,0,2h1a1,1,0,0,0,0-2Zm-9,8a1,1,0,0,0-1,1v1a1,1,0,0,0,2,0V20A1,1,0,0,0,12,19ZM18.36,17A1,1,0,0,0,17,18.36l.71.71a1,1,0,0,0,1.41,0,1,1,0,0,0,0-1.41ZM12,6.5A5.5,5.5,0,1,0,17.5,12,5.51,5.51,0,0,0,12,6.5Zm0,9A3.5,3.5,0,1,1,15.5,12,3.5,3.5,0,0,1,12,15.5Z" />
+              </svg>
+
+              {/* moon icon */}
+              <svg
+                className="swap-off fill-current w-6 h-6"
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 24 24">
+                <path d="M21.64,13a1,1,0,0,0-1.05-.14,8.05,8.05,0,0,1-3.37.73A8.15,8.15,0,0,1,9.08,5.49a8.59,8.59,0,0,1,.25-2A1,1,0,0,0,8,2.36,10.14,10.14,0,1,0,22,14.05,1,1,0,0,0,21.64,13Zm-9.5,6.69A8.14,8.14,0,0,1,7.08,5.22v.27A10.15,10.15,0,0,0,17.22,15.63a9.79,9.79,0,0,0,2.1-.22A8.11,8.11,0,0,1,12.14,19.73Z" />
+              </svg>
+            </label>
             <Link
               to="/"
               className="cursor-pointer hover:text-accent-focus normal-case font-semibold text-[1rem] hidden md:inline-block">
@@ -159,6 +187,7 @@ const Navbar = () => {
               Logout
             </a>
           )}
+
           {/* Cart */}
           <div className="dropdown dropdown-end">
             <label tabIndex={0} className="btn btn-ghost btn-circle">

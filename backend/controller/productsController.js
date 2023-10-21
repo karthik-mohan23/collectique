0a53c6e0a48f1e34df65169beea605acc2b3583b
karthik-mohan23 <karthikmohan2310@gmail.com>
2023-10-21 09:23:42 +0530

    created contoller functions for creating,updating and deleting products

--- backend/controller/productsController.js
+++ backend/controller/productsController.js
@@ -18,10 +18,97 @@ const getAllProducts = async (req, res, next) => {
 const getSingleProduct = async (req, res, next) => {
   try {
     const singleProduct = await productsModel.findById(req.params.id);
-    res.status(200).json(singleProduct);
+    if (singleProduct) {
+      res.status(200).json(singleProduct);
+    } else {
+      res.status(400).json({ message: "Cannot find product" });
+    }
   } catch (error) {
-    console.log(error);
+    res.status(400).json({ message: "Something went wrong" });
   }
 };
 
-module.exports = { getAllProducts, getSingleProduct };
+// @desc    Create a product
+// @route   POST /api/products
+// @access  Private/Admin
+const createProduct = async (req, res) => {
+  const { newProduct } = req.body;
+
+  if (newProduct) {
+    res.status(400).json({ message: "Fill all fields" });
+  }
+
+  try {
+    const createdProduct = await product.create(newProduct);
+    res.status(201).json(createdProduct);
+  } catch (error) {
+    res.status(400).json({ message: "Something went wrong" });
+  }
+};
+
+// @desc    Update a product
+// @route   PUT /api/products/:id
+// @access  Private/Admin
+const updateProduct = async (req, res) => {
+  try {
+    const {
+      name,
+      price,
+      image,
+      seller,
+      category,
+      countInStock,
+      numReviews,
+      description,
+      assured,
+      rating,
+    } = req.body;
+
+    const product = await Product.findById(req.params.id);
+
+    if (product) {
+      product.name = name;
+      product.price = price;
+      product.description = description;
+      product.image = image;
+      product.seller = seller;
+      product.category = category;
+      product.countInStock = countInStock;
+      product.numReviews = numReviews;
+      product.assured = assured;
+      product.rating = rating;
+
+      const updatedProduct = await product.save();
+      res.json(updatedProduct);
+    } else {
+      res.status(404);
+      throw new Error("Product not found");
+    }
+  } catch (error) {
+    res.status(400).json({ message: "Something went wrong" });
+  }
+};
+
+// @desc    Delete single product by id
+// @route   DELETE /api/products/:id
+// @access  Private/Admin
+const deleteProduct = async (req, res, next) => {
+  try {
+    const singleProduct = await productsModel.findByIdAndDelete(req.params.id);
+    if (singleProduct) {
+      res.status(200).json(singleProduct);
+    } else {
+      res.status(400).json({ message: "Cannot find product" });
+    }
+  } catch (error) {
+    res.status(400).json({ message: "Something went wrong" });
+  }
+};
+
+module.exports = {
+  getAllProducts,
+  getSingleProduct,
+  createProduct,
+  updateProduct,
+  deleteProduct,
+};
--- backend/routes/productsRoute.js
+++ backend/routes/productsRoute.js
@@ -1,8 +1,12 @@
 const router = require("express").Router();
-
+// to protect routes
+const { protect, admin } = require("../middlewares/authMiddleware");
 const {
   getAllProducts,
   getSingleProduct,
+  createProduct,
+  updateProduct,
+  deleteProduct,
 } = require("../controller/productsController");
 
 // to get all products
@@ -10,5 +14,8 @@ router.get("/", getAllProducts);
 
 // to get single product
 router.get("/:id", getSingleProduct);
+router.post("/", protect, admin, createProduct);
+router.put("/:id", protect, admin, updateProduct);
+router.delete("/:id", protect, admin, deleteProduct);
 
 module.exports = router;

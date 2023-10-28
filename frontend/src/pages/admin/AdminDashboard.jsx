7772cf2a87906f2c61c219af79afab3bcf9cfae5
karthik-mohan23 <karthikmohan2310@gmail.com>
2023-10-28 16:17:49 +0530

    updated styling of admin dashboard

--- frontend/src/pages/admin/AdminDashboard.jsx
+++ frontend/src/pages/admin/AdminDashboard.jsx
@@ -42,10 +42,10 @@ const AdminDashboard = () => {
   return (
     <section className="min-h-screen ">
       <div className="w-[90%] max-w-5xl mx-auto ">
-        <div className="w-[90%] max-w-2xl mx-auto grid grid-cols-1 gap-4 md:grid-cols-2 pt-40 ">
+        <div className="w-[90%] max-w-2xl mx-auto grid grid-cols-1 gap-4 md:grid-cols-2 pt-20 pb-20 md:pt-40 md:pb-0 ">
           {/* users */}
           <Link to="/admin/active-users">
-            <div className=" h-32 rounded-xl bg-yellow-300  text-black text-2xl flex justify-center items-center hover:bg-yellow-500 duration-300 hover:cursor-pointer">
+            <div className=" h-32 bg-gradient-to-r from-cyan-500 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-cyan-300 dark:focus:ring-cyan-800 rounded-lg  text-white text-2xl flex justify-center items-center duration-1000 font-medium  hover:cursor-pointer">
               {activeUsers.length > 1
                 ? `${activeUsers.length} Active users`
                 : `${activeUsers.length} Active user`}
@@ -54,7 +54,7 @@ const AdminDashboard = () => {
 
           {/* Admin users */}
           <Link to="/admin/admin-users">
-            <div className=" h-32 rounded-xl bg-violet-400  text-black text-2xl flex justify-center items-center hover:bg-violet-300 duration-300 hover:cursor-pointer">
+            <div className=" h-32 rounded-xl bg-gradient-to-r from-purple-500 to-pink-500 hover:bg-gradient-to-l focus:ring-4 focus:outline-none focus:ring-purple-200 dark:focus:ring-purple-800  text-white text-2xl flex justify-center items-center duration-1000 font-medium hover:cursor-pointer">
               {adminUsers.length > 1
                 ? `${adminUsers.length} Admin users`
                 : `${adminUsers.length} Admin user`}
@@ -63,7 +63,7 @@ const AdminDashboard = () => {
 
           {/* orders */}
           <Link to="/admin/order-records">
-            <div className="h-32 rounded-xl bg-pink-500  text-black text-2xl flex justify-center items-center hover:bg-pink-300 duration-300 hover:cursor-pointer">
+            <div className="h-32 rounded-xl bg-gradient-to-br from-pink-500 to-orange-400 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-pink-200 dark:focus:ring-pink-800  text-white text-2xl flex justify-center items-center font-medium duration-1000 hover:cursor-pointer">
               {pendingOrders.length === 0
                 ? `All orders delivered`
                 : pendingOrders.length === 1
@@ -74,7 +74,7 @@ const AdminDashboard = () => {
 
           {/* products */}
           <Link to="/admin/product-management">
-            <div className="h-32 rounded-xl bg-green-400  text-black text-2xl flex justify-center items-center hover:bg-green-300 duration-300 hover:cursor-pointer">
+            <div className="h-32 rounded-xl bg-gradient-to-br from-green-400 to-blue-600 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-green-200 dark:focus:ring-green-800 text-white text-2xl flex justify-center items-center duration-1000 font-medium hover:cursor-pointer">
               {allProducts.length === 0
                 ? `No products`
                 : allProducts.length === 1
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -2,11 +2,7 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {
-      // fontFamily: {
-      //   custom: ["Inter", "sans-serif"],
-      // },
-    },
+    extend: {},
   },
   plugins: [require("@tailwindcss/typography"), require("daisyui")],
   daisyui: {

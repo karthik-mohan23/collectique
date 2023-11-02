3a6ad758a5c6c5e6cf475afbec64cf28f1aa691f
karthik-mohan23 <karthikmohan2310@gmail.com>
2023-11-02 16:30:33 +0530

    setup for production

--- backend/server.js
+++ backend/server.js
@@ -16,6 +16,7 @@ app.use(express.urlencoded({ extended: true }));
 // to parse cookie from request object
 // allows to access req.cookies
 app.use(cookieParser());
+
 // to access images
 app.use("/uploads", express.static("uploads"));
 
@@ -36,6 +37,15 @@ app.use("/api/upload", uploadRoutes);
 // razorpay key
 app.get("/api/razorpay", (req, res) =>
   res.send({ clientId: process.env.KEY_ID })
+);
+
+// production
+const __dirname = path.resolve();
+
+app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+app.get("*", (req, res) =>
+  res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"))
 );
 
 app.listen(process.env.PORT, () => {
